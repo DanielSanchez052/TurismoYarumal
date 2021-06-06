bedf573377512891e9ef97177d3f6c207b791a83
Daniel Sanchez <sanchezd0528@gmail.com>
2021-06-05 20:38:27 -0500

    se retira el menu de detalleLugar
    se retira el menú de detalleLugar ya que este generaba problemas en la aplicación

--- app/src/main/java/com/example/turismoyarumal/DetalleLugar.java
+++ app/src/main/java/com/example/turismoyarumal/DetalleLugar.java
@@ -46,40 +46,4 @@ public class DetalleLugar extends AppCompatActivity {
                 .into(imagenDetalle);
         //imagenDetalle.setImageResource(lugarDetalle.getImagenElemento());
     }
-
-    public boolean onCreateOptionsMenu (Menu menu){
-        getMenuInflater().inflate(R.menu.main_menu,menu);
-
-        return true;
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id = item.getItemId();
-
-        switch (id){
-            case(R.id.opcion1):
-                cambiarIdioma("en");
-                Intent intent1 = new Intent(DetalleLugar.this, DetalleLugar.class);
-                startActivity(intent1);
-                break;
-            case(R.id.opcion2):
-                cambiarIdioma("es");
-                Intent intent2 = new Intent(DetalleLugar.this, DetalleLugar.class);
-                startActivity(intent2);
-                break;
-            case(R.id.opcion3):
-                Intent intent4 = new Intent(DetalleLugar.this,AcercaDe.class);
-                startActivity(intent4);
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    public void cambiarIdioma(String lenguaje){
-        Locale idioma = new Locale(lenguaje);
-        Locale.setDefault(idioma);
-
-        Configuration configuracionTelefono = getResources().getConfiguration();
-        configuracionTelefono.locale=idioma;
-        getBaseContext().getResources().updateConfiguration(configuracionTelefono,getBaseContext().getResources().getDisplayMetrics());
-    }
 }
